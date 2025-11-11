5c287eac98026515607dacb4790d26a96eff617e
Mark Laurence Lawis <lawis32918@gmail.com>
2025-11-11 14:56:29 +0800

    11/11/2025 Updates

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,16 +4,16 @@ const Resume = () => {
   return (
     <div className="py-20">
       <div>
-        <a href="./Mark_Lawis_CV.pdf" download>
+        <a href="/Mark_Lawis_CV.pdf" download>
           <Button size="sm">Download CV</Button>
         </a>
       </div>
       <div className="w-full justify-items-center mt-8">
         <div className="w-[60%]">
           <img
-            // className="object-contain"
             src="/Mark_Lawis_CV.jpg"
             alt="My_Resume"
+            onContextMenu={(e) => e.preventDefault()}
           />
         </div>
       </div>
